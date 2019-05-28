3e441168b9240db7fd0a8127dcf929162b01b8ee
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2019-05-28 23:38:02 +0100

    bump version to 1.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='pipelinewise-tap-mysql',
-      version='1.0.4',
+      version='1.0.5',
       description='Singer.io tap for extracting data from MySQL - PipelineWise compatible',
       author='Stitch',
       url='https://github.com/transferwise/pipelinewise-tap-mysql',
