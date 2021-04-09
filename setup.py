4f0732bff5c8417832dec71cf3df0a28d13a5b84
Samira El Aabidi <54845154+Samira-El@users.noreply.github.com>
2021-04-09 14:34:44 +0300

    AP-859 [Follow up] - Add unit tests for binlog & rearrange tests (#63)
    * binlog unit tests
    
    * move integration tests to folder
    
    * update nosetest settings
    
    * update dependencies
    
    * update ci

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(name='pipelinewise-tap-mysql',
       ],
       py_modules=['tap_mysql'],
       install_requires=[
-          'pendulum==1.2.0',
+          'pendulum==1.5.1',
           'pipelinewise-singer-python==1.*',
           'PyMySQL==0.7.11',
           'mysql-replication==0.23',
