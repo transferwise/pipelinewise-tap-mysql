3cb0be5540a31895ff42e740abb1f3d242c70387
Samira El Aabidi <54845154+Samira-El@users.noreply.github.com>
2022-04-05 10:05:07 +0300

    Release 1.5.1 (#103)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+1.5.1 (2022-04-04)
+------------------
+* Fix: Handle case when BINLOG_GTID_POS returns multiple comma separated GTIDs
+
+
 1.5.0 (2022-03-11)
 ------------------
 * Support logical replication using GTID, for both Mariadb & MySql 
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='pipelinewise-tap-mysql',
-      version='1.5.0',
+      version='1.5.1',
       description='Singer.io tap for extracting data from MySQL & MariaDB - PipelineWise compatible',
       long_description=long_description,
       long_description_content_type='text/markdown',
